4be76236450bf600d2176def92ee389ca77e95e1
liza-kl <l.kletsko0202@gmail.com>
2020-11-08 13:02:16 +0100

    :lipstick: got rid of link underline

--- src/components/cardlist/card.components.jsx
+++ src/components/cardlist/card.components.jsx
@@ -3,9 +3,9 @@ import 'tachyons';
 import {Route, BrowserRouter as Router, Link} from "react-router-dom";
 const Card = ({ topic, description }) => {
     return (
-        <Link to={`/${topic}`} className='tc grow bg-washed-blue br3 pa3 ma2 dib bw2 shadow-5'>
+        <Link style={{ textDecoration: 'none' }} to={`/${topic}`} className='tc grow bg-gray br3 pa3 ma2 dib bw2 shadow-5'>
             <div>
-                <h2>{topic}</h2>
+                <h2 className="black">{topic}</h2>
                 <p>{description}</p>
             </div>
         </Link>
